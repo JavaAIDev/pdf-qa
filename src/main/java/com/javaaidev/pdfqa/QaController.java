d4c4b0c9f12f7ec4bd3c24dea3a60809979464c9
Fu Cheng <alexcheng1982@gmail.com>
2025-04-16 22:23:50 +1200

    use Chat Agent UI

--- src/main/java/com/javaaidev/pdfqa/QaController.java
+++ src/main/java/com/javaaidev/pdfqa/QaController.java
@@ -1,11 +1,17 @@
 package com.javaaidev.pdfqa;
 
+import com.javaaidev.chatagent.model.ChatAgentRequest;
+import com.javaaidev.chatagent.model.ChatAgentResponse;
+import com.javaaidev.chatagent.springai.ModelAdapter;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class QaController {
@@ -20,16 +26,13 @@ public class QaController {
         simpleLoggerAdvisor).build();
   }
 
-  @PostMapping("/qa")
-  public QaResponse qa(@RequestBody QaRequest request) {
-    return new QaResponse(chatClient.prompt().user(request.input()).call().content());
+  @PostMapping("/chat")
+  public Flux<ServerSentEvent<ChatAgentResponse>> qa(@RequestBody ChatAgentRequest request) {
+    return ModelAdapter.toStreamingResponse(
+        chatClient.prompt()
+            .messages(ModelAdapter.fromRequest(request).toArray(new Message[0]))
+            .stream()
+            .chatResponse());
   }
 
-  public record QaRequest(String input) {
-
-  }
-
-  public record QaResponse(String output) {
-
-  }
 }
